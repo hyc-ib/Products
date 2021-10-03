9c5ee501d4b774bccee3f3d8b6bc2477ed750d93
Hsin Yu <winter0462@gmail.com>
2021-10-03 22:11:06 +0800

    add for loop

--- products.py
+++ products.py
@@ -6,3 +6,8 @@ while True:
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
 print(products)
+
+print('--------------------')
+
+for product in products:
+	print(product[0], '的價格是', product[1])
