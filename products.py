89d058ac98e8fe6b40404ec568507d390db40813
Hsin Yu <winter0462@gmail.com>
2021-10-07 20:34:35 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue # 進入下一個迴圈
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 執行碼
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -8,10 +18,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 購買紀錄
 for product in products:
 	print(product[0], '的價格是', product[1])
 
-
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for product in products:
