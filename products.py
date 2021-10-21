b6b8fe698b19d0c232e95e182d96e8d72ac1a28f
Hsin Yu <winter0462@gmail.com>
2021-10-21 17:09:37 +0800

    bug fix and refactor

--- products.py
+++ products.py
@@ -2,35 +2,53 @@
 import os
 
 # 讀取檔案
-products = []
-if os.path.isfile('products.csv'): # 檢查檔案在不在
-	print('繼續輸入')
-	with open('products.csv', 'r', encoding='utf-8') as f:
-		for line in f:
-			if '商品,價格' in line:
-				continue # 進入下一個迴圈
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('還沒有歷史紀錄')
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 進入下一個迴圈
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
 
 # 執行碼
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'Q' or 'q':
-		break
-	price = input('請輸入商品價格: ')
-	price = int(price)
-	products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'Q' and 'q':
+            break
+        price = input('請輸入商品價格: ')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 購買紀錄
-for product in products:
-	print(product[0], '的價格是', product[1])
+def print_products(products):
+    for product in products:
+        print(product[0], '的價格是', product[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for product in products:
-		f.write(product[0] + ',' + str(product[1]) + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for product in products:
+            f.write(product[0] + ',' + str(product[1]) + '\n')
+
+# main function
+def main():
+    products = [] # 如果沒有找到檔案也有製造清單
+    filename = 'products.csv'
+    if os.path.isfile(filename): # 檢查檔案在不在
+        print('繼續輸入')
+        products = read_file(filename)
+    else:
+        print('還沒有歷史紀錄')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+
+main()
